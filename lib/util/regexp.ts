bb6ecee254428d6b2eb013d73055651c3af9a266
Richard Harris <rjh@netcraft.com>
2020-04-27 16:45:36 +0100

    Add rx, a function for multiline, commented regexes

--- lib/util/regexp.ts
+++ lib/util/regexp.ts
@@ -9,3 +9,34 @@ export function buildAlternationRaw(words: string[]): string {
 export function buildAlternation(words: string[], flags?: string): RegExp {
     return new RegExp(buildAlternationRaw(words), flags);
 }
+
+// Template string allowing multiline regexes with comments
+export function rx(flags) { 
+    const trailingComments = /\s+#.*$/gm;
+    const surroundingWhitespace = /^\s+|\s+$/gm;
+    const literalNewlines = /[\r\n]/g;
+
+    return (strings, ...values) => { 
+        function toPattern(pattern, rawString, i) { 
+            var value = values[i];
+
+            if (value == null) { 
+                return pattern + rawString;
+            }
+
+            if (value instanceof RegExp) { 
+                value = value.source;
+            }
+
+            return pattern + rawString + value;
+        }
+
+        const compiledPattern = strings.raw
+            .reduce(toPattern, '')
+            .replace(trailingComments, '')
+            .replace(surroundingWhitespace, '')
+            .replace(literalNewlines, '');
+
+        return new RegExp(compiledPattern, flags);
+    };
+}
